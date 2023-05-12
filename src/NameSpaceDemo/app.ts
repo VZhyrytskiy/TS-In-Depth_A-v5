cb57214ae60fdca80d24ecf06bc7b7b8a41e299a
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-05-12 18:22:57 +0300

    fix: fix functions names

--- src/NameSpaceDemo/app.ts
+++ src/NameSpaceDemo/app.ts
@@ -1,9 +1,9 @@
 /// <reference path="utility-functions.ts" />
 
-const num = Utility.MaxBooksAllowed(20);
+const num = Utility.maxBooksAllowed(20);
 console.log(num);
 
 import util = Utility.Fees;
 
-let fee = util.CalculateLateFee(10);
+let fee = util.calculateLateFee(10);
 console.log(`Fee: ${fee}`);
